f859125e8bbfffce6386e0b2df4cb8bdaf7d5557
Nat Ballou <natbal@gmail.com>
2025-07-03 12:33:52 -0700

    Remove pyrobosim copy

--- src/pyrobosim/pyrobosim_ros/pyrobosim_ros/__init__.py
+++
@@ -1,5 +0,0 @@
-"""ROS 2 interface to PyRoboSim.
-
-This module contains tools for using PyRoboSim with ROS 2, including interfaces
-to world and robot models, as well as general conversion utilities.
-"""
